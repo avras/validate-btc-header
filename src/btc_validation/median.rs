cd24fb1484fd21e8b06cc9c45ffb53c59739f0f1
E-Kritheesh <e.kritheesh@gmail.com>
2024-02-16 02:26:06 +0530

    Added the less than or equal to condition in the return statement of function verify_median_timestamp, added comments, and replaced the local equals function by its BigNat equivalent

--- src/btc_validation/median.rs
+++ src/btc_validation/median.rs
@@ -4,6 +4,7 @@ use bellpepper_core::{ConstraintSystem, SynthesisError, test_cs::TestConstraintS
 use bellpepper::gadgets::num::{AllocatedNum, Num};
 use ff::PrimeField;
 use crate::util::num;
+use crate::mp::bignat::BigNat;
 
 pub fn compute_median_timestamp (prev_timestamps: &mut Vec<u32>) -> u32
 {
@@ -29,12 +30,13 @@ CS: ConstraintSystem<Scalar>,
     let mut sign_diff = AllocatedNum::alloc(cs.namespace(|| "sign_diff"), || Ok(Scalar::from(0u64))).unwrap();
 
     for (i, fe_time) in fe_timestamps.iter().enumerate() {
-        let res_eq = equals(cs.namespace(|| format!("check equal for time {}", i)), &fe_median, fe_time).unwrap(); //.get_value().unwrap();
-        // n_median_occurrences += result as u32;
+        // n_median_occurrences += 1 if median == timestamp else no change
+        let res_eq = BigNat::equals(cs.namespace(|| format!("check equal for time {}", i)), &fe_median, fe_time).unwrap();
         n_median_occurrences = n_median_occurrences.add_bool_with_coeff(CS::one(), &res_eq, Scalar::ONE);
 
         let res_lt = less_than(&fe_median, fe_time, 32).unwrap();
 
+        // delta_sign is the signum of (median - timestamp)
         let delta_sign = AllocatedNum::alloc(cs.namespace(|| format!("signum {}", i)), || {
             let eq = Scalar::from(res_eq.get_value().unwrap() as u64);
             let lt = Scalar::from(res_lt.get_value().unwrap() as u64);
@@ -62,9 +64,10 @@ CS: ConstraintSystem<Scalar>,
             |lc| lc + delta_sign.get_variable(),
         );
 
+        // sign_diff += signum(median - timestamp)
         sign_diff = sign_diff.add(cs.namespace(|| format!("sign_diff increment {}", i)), &delta_sign).unwrap();
     }
-    // absolute value needs to be obtained
+    // absolute value of sign_diff needs to be obtained
     let const_12 = AllocatedNum::alloc(cs.namespace(|| "max positive sign_diff"), || Ok(Scalar::from(12u64))).unwrap();
     let is_positive = less_than(&sign_diff, &const_12, 5usize).unwrap();
     let neg_sign_diff = AllocatedNum::alloc(cs.namespace(|| "Negative of sign_diff"), || {
@@ -86,10 +89,13 @@ CS: ConstraintSystem<Scalar>,
 
     let const_1 = AllocatedNum::alloc(cs.namespace(|| "increment by 1"), || Ok(Scalar::ONE)).unwrap();
     let lhs = abs_sign_diff.add(cs.namespace(|| "LHS of inequality"), &const_1).unwrap();
+    // lhs = abs(sign_diff) + 1
 
-    let fe_median = AllocatedNum::alloc(cs.namespace(|| "fe_median"), || Ok(n_median_occurrences.get_value().unwrap())).unwrap();
+    let fe_median_occ = AllocatedNum::alloc(cs.namespace(|| "fe_median"), || Ok(n_median_occurrences.get_value().unwrap())).unwrap();
 
-    equals(cs.namespace(|| "verify median"), &lhs, &fe_median)   
+    let r_eq = BigNat::equals(cs.namespace(|| "verify median"), &lhs, &fe_median_occ).unwrap();
+    let r_lt = less_than(&lhs, &fe_median_occ, 5).unwrap();
+    Boolean::or(cs.namespace(|| "less than or equal to check"), &r_eq, &r_lt)   
 }
 
 /// Takes two allocated numbers (a, b) and returns
@@ -123,112 +129,6 @@ where
     Ok(Boolean::from(r))
 }
 
-// from bignat.rs
-
-    /// Takes two allocated numbers (a, b) and returns
-    /// allocated boolean variable with value `true`
-    /// if the `a` and `b` are equal, `false` otherwise.
-    pub fn equals<Scalar, CS>(
-        mut cs: CS,
-        a: &AllocatedNum<Scalar>,
-        b: &AllocatedNum<Scalar>,
-    ) -> Result<Boolean, SynthesisError>
-    where
-        Scalar: PrimeField,
-        CS: ConstraintSystem<Scalar>,
-    {
-        // Allocate and constrain `r`: result boolean bit.
-        // It equals `true` if `a` equals `b`, `false` otherwise
-
-        let r_value = match (a.get_value(), b.get_value()) {
-            (Some(a), Some(b)) => Some(a == b),
-            _ => None,
-        };
-
-        let r = AllocatedBit::alloc(cs.namespace(|| "r"), r_value)?;
-
-        let delta = AllocatedNum::alloc(cs.namespace(|| "delta"), || {
-            let a_value = a.get_value().unwrap();
-            let b_value = b.get_value().unwrap();
-
-            let mut delta = a_value;
-            delta.sub_assign(&b_value);
-
-            Ok(delta)
-        })?;
-
-        //
-        cs.enforce(
-            || "delta = (a - b)",
-            |lc| lc + a.get_variable() - b.get_variable(),
-            |lc| lc + CS::one(),
-            |lc| lc + delta.get_variable(),
-        );
-
-        let delta_inv = AllocatedNum::alloc(cs.namespace(|| "delta_inv"), || {
-            let delta = delta.get_value().unwrap();
-
-            if delta.is_zero().unwrap_u8() == 1 {
-                Ok(Scalar::ONE) // we can return any number here, it doesn't matter
-            } else {
-                Ok(delta.invert().unwrap())
-            }
-        })?;
-
-        // Allocate `t = delta * delta_inv`
-        // If `delta` is non-zero (a != b), `t` will equal 1
-        // If `delta` is zero (a == b), `t` cannot equal 1
-
-        let t = AllocatedNum::alloc(cs.namespace(|| "t"), || {
-            let mut tmp = delta.get_value().unwrap();
-            tmp.mul_assign(&(delta_inv.get_value().unwrap()));
-
-            Ok(tmp)
-        })?;
-
-        // Constrain allocation:
-        // t = (a - b) * delta_inv
-        cs.enforce(
-            || "t = (a - b) * delta_inv",
-            |lc| lc + a.get_variable() - b.get_variable(),
-            |lc| lc + delta_inv.get_variable(),
-            |lc| lc + t.get_variable(),
-        );
-
-        // Constrain:
-        // (a - b) * (t - 1) == 0
-        // This enforces that correct `delta_inv` was provided,
-        // and thus `t` is 1 if `(a - b)` is non zero (a != b )
-        cs.enforce(
-            || "(a - b) * (t - 1) == 0",
-            |lc| lc + a.get_variable() - b.get_variable(),
-            |lc| lc + t.get_variable() - CS::one(),
-            |lc| lc,
-        );
-
-        // Constrain:
-        // (a - b) * r == 0
-        // This enforces that `r` is zero if `(a - b)` is non-zero (a != b)
-        cs.enforce(
-            || "(a - b) * r == 0",
-            |lc| lc + a.get_variable() - b.get_variable(),
-            |lc| lc + r.get_variable(),
-            |lc| lc,
-        );
-
-        // Constrain:
-        // (t - 1) * (r - 1) == 0
-        // This enforces that `r` is one if `t` is not one (a == b)
-        cs.enforce(
-            || "(t - 1) * (r - 1) == 0",
-            |lc| lc + t.get_variable() - CS::one(),
-            |lc| lc + r.get_variable() - CS::one(),
-            |lc| lc,
-        );
-
-        Ok(Boolean::from(r))
-    }
-
 
 #[cfg(test)]
 mod tests {
@@ -246,13 +146,13 @@ mod tests {
 
     #[test]
     fn test_median_verify() {
-        let cs = TestConstraintSystem::<Fr>::new();
-        // let mut timestamps: Vec<u32> = vec![11,2,3,4,7,6,8,6,10,9,1];
-        let mut timestamps: Vec<u32> = vec![111,52,93,74,27,26,28,26,10,49,51];
+        let mut cs = TestConstraintSystem::<Fr>::new();
+        let mut timestamps: Vec<u32> = vec![11,2,3,4,6,6,8,6,10,9,1];
+        // let mut timestamps: Vec<u32> = vec![11,11,11,11,11,11,11,11,11,11,11,11];
         let median: u32 = compute_median_timestamp(&mut timestamps);
 
-        let r = verify_median_timestamp(cs, &mut timestamps, median).unwrap().get_value().unwrap();
-        assert_eq!(r, true);
+        let r = verify_median_timestamp(cs.namespace(|| "verify median"), &mut timestamps, median).unwrap().get_value().unwrap();
+        assert!(r);
     }
 
     #[test]
@@ -261,7 +161,7 @@ mod tests {
 
         let c = AllocatedNum::alloc(cs.namespace(|| "c"), || Ok(Fr::from(4u64))).unwrap();
         let d = AllocatedNum::alloc(cs.namespace(|| "d"), || Ok(Fr::from(4u64))).unwrap();
-        let r2 = equals(cs.namespace(|| "check equal"), &c, &d).unwrap().get_value().unwrap();
+        let r2 = BigNat::equals(cs.namespace(|| "check equal"), &c, &d).unwrap().get_value().unwrap();
 
         assert_eq!(r2, true);
     }
