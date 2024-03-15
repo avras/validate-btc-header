a9c4dba034c65bbe4fd894ade697af359eb47dea
E-Kritheesh <e.kritheesh@gmail.com>
2024-03-15 09:00:21 +0530

    Changed synthesize function

--- src/btc_validation/header_step.rs
+++ src/btc_validation/header_step.rs
@@ -3,11 +3,12 @@ use std::marker::PhantomData;
 use crate::btc_validation::{difficulty_update, hash_target, median, prev_block_hash};
 
 use bellpepper_core::{
-    // boolean::{AllocatedBit, Boolean},
+    boolean,
     num::AllocatedNum,
     ConstraintSystem, SynthesisError,
 };
 use ff::{PrimeField, PrimeFieldBits};
+use bellpepper::gadgets::sha256;
 use nova_snark::traits::circuit::StepCircuit;
 
 #[derive(Clone, Debug)]
@@ -48,13 +49,41 @@ where
         cs: &mut CS,
         z: &[AllocatedNum<F>],
     ) -> Result<Vec<AllocatedNum<F>>, SynthesisError> {
-        let header = self.msg_block;
-        let prev_hash = &header[4..36];
-        // let nTime = &header[68..72];
-        let threshold = &header[72..76];
+        // z contains previous block to hash maybe ??
+        // prev. target and t_sum for difficulty update also in z?
+
+        let header = self.msg_block.to_vec();
+        
+        let mut preimage_vec: Vec<boolean::Boolean> = Vec::new();
+        for (i, preimage64) in header[4..36].iter().enumerate() {
+            let mut dummy2 = boolean::u64_into_boolean_vec_le(cs.namespace(|| format!("dummy2 {}", i)), Some(*preimage64 as u64)).unwrap();
+            dummy2.reverse();
+            dummy2 = dummy2[0..8].to_vec();
+            preimage_vec.append(&mut dummy2);
+        }
+
+        let out_sha256 = sha256::sha256(cs.namespace(|| "SHA 256"), &preimage_vec).unwrap();
+        let out_sha256d = sha256::sha256(cs.namespace(|| "SHA 256d"), &out_sha256).unwrap();
+        
+        let mut threshold = header[73..76].to_vec();
+        let exp: usize = (header[72] as usize) - 16 - 3;
+        let mut trail_zeros = vec![0u8; exp];
+        threshold.append(&mut trail_zeros);
+
+        let target: u128 = threshold.iter()
+                            .enumerate()
+                            .map(|(i,b)| ((1 << i as u128) * (*b as u128) as u128))
+                            .sum();
+
+        // let r2 = verify_current_hash(cs.namespace(|| "trivial"), 
+        //                                 hash_u, 
+        //                                 hash_l, 
+        //                                 target, 
+        //                                 0u128).unwrap();
+
         let mut timestamps = self.prev_timestamps.to_vec();
 
-        let res_median = median::verify_median_timestamp(cs.namespace(|| "median check"), &mut timestamps, self.median).unwrap();
+        let r1 = median::verify_median_timestamp(cs.namespace(|| "median check"), &mut timestamps, self.median).unwrap();
 
         return Ok((*z).to_vec())
     }
